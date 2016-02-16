78bedad7917eb9092d30e6b6f8b0a0afd6e62a02
kentsay <naturen@gmail.com>
2016-02-17 00:44:22 +0100

    init LineItem data checking

---
+++ src/test/java/ch/ethz/system/mt/tpch/verify/LineItemsTblDataCheck.java
@@ -0,0 +1,18 @@
+package ch.ethz.system.mt.tpch.verify;
+
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LineItemsTblDataCheck {
+
+    @BeforeSuite
+    public void init() {
+
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
